b2bac80bf18c79f44f3227472e73eb5dc2b73113
FedorovskihSimon <fedorovskihsemen@gmail.com>
2025-03-09 01:18:29 +0700

    part 6 completed

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -3,16 +3,28 @@ import readlineSync from 'readline-sync';
 import { askingName, getRandomValue, getRandomInt } from '../index.js';
 
 console.log('Welcome to the Brain Games!');
-const username = askingName();
-console.log('What is the result of the expression?');
 
 const array = ['*', '-', '+'];
+const username = askingName()
 let i = 0;
 let randomExpression = `${getRandomInt(1, 50)} ${getRandomValue(array)} ${getRandomInt(1, 50)}`;
+const correctAnswer = eval(randomExpression);
 
 while (i < 3) {
   console.log(`Question: ${randomExpression}`);
   const answer = readlineSync.question('Your answer: ');
+
+  if (Number(answer) !== eval(randomExpression)) {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    console.log(`Let's try again, ${username}!`);
+    break;
+  }
+
+  console.log('Correct!');
   i += 1;
   randomExpression = `${getRandomInt(1, 50)} ${getRandomValue(array)} ${getRandomInt(1, 50)}`;
+
+  if (i === 3) {
+    console.log(`Congratulations, ${username}!`);
+  }
 }
