1fae38b0c18560df5e637ef73e9404303f070fcb
FedorovskihSimon <fedorovskihsemen@gmail.com>
2025-05-13 18:35:31 +0700

    deleted semicolons

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,44 +1,44 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import { askingName, getRandomInt } from '../index.js';
+import readlineSync from 'readline-sync'
+import { askingName, getRandomInt } from '../index.js'
 
-console.log('Welcome to the Brain Games!');
-const username = askingName();
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+console.log('Welcome to the Brain Games!')
+const username = askingName()
+console.log('Answer "yes" if the number is even, otherwise answer "no".')
 
-let randomNumber = getRandomInt(1, 50);
-let i = 0;
-let trueness;
-let correctAnswer;
+let randomNumber = getRandomInt(1, 50)
+let i = 0
+let trueness
+let correctAnswer
 
 while (i < 3) {
-  console.log(`Question: ${randomNumber}`);
-  const answer = readlineSync.question('Your answer: ');
-  const isEven = randomNumber % 2 === 0;
+  console.log(`Question: ${randomNumber}`)
+  const answer = readlineSync.question('Your answer: ')
+  const isEven = randomNumber % 2 === 0
 
   switch (answer) {
     case 'yes':
-      trueness = true;
-      correctAnswer = 'no';
-      break;
+      trueness = true
+      correctAnswer = 'no'
+      break
     case 'no':
-      trueness = false;
-      correctAnswer = 'yes';
-      break;
+      trueness = false
+      correctAnswer = 'yes'
+      break
     default:
-      correctAnswer = 'no';
+      correctAnswer = 'no'
   }
   if (isEven === trueness) {
-    console.log('Correct!');
+    console.log('Correct!')
   }
   if (isEven !== trueness) {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-    console.log(`Let's try again, ${username}!`);
-    break;
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+    console.log(`Let's try again, ${username}!`)
+    break
   }
-  i += 1;
-  randomNumber = getRandomInt(1, 50);
+  i += 1
+  randomNumber = getRandomInt(1, 50)
   if (i === 3) {
-    console.log(`Congratulations, ${username}!`);
+    console.log(`Congratulations, ${username}!`)
   }
 }
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import { askingName } from '../index.js';
+import { askingName } from '../index.js'
 
-console.log('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!')
 
-askingName();
+askingName()
